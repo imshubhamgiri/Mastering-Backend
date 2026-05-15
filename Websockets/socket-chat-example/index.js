3586e07cae9985c7d5a8cf30a307697b11173621
imshubhamgiri <skgiri569@gmail.com>
2026-05-15 22:29:19 +0530

    add db and checking connections state and send message in synchronized order after a connection drop or reconnection

--- Websockets/socket-chat-example/index.js
+++ Websockets/socket-chat-example/index.js
@@ -1,10 +1,8 @@
 import express from 'express';
-// import socketio from 'socket.io';
 import { createServer } from 'http';
 import path from 'path';
 import {Server} from 'socket.io';
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+import { connectDB, Message } from './src/db.js';
 
 const app = express();
 const PORT = 3000;
@@ -16,18 +14,49 @@ const io = new Server(server,{
 app.use(express.static(path.join(process.cwd(), 'public')));
 
 
+
 app.get('/', (_req, res) => {
     res.sendFile(path.join(process.cwd(), 'public', 'index.html'));
 });
 
-io.on('connection', (socket) => {
+// Connect to MongoDB
+connectDB();
+
+io.on('connection', async (socket) => {
   console.log('a user connected' , socket.id);
+  
+  // When connection drops (or page refresh/manual connect), fetch missed messages
+  if (!socket.recovered) {
+    try {
+      const serverOffset = socket.handshake.auth.serverOffset;
+      const query = serverOffset ? { _id: { $gt: serverOffset } } : {};
+      
+      const missedMessages = await Message.find(query);
+      missedMessages.forEach((doc) => {
+        // Send previous messages and their database _id as the offset
+        socket.emit('chat message', doc.content, doc._id.toString());
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
   socket.on('disconnect', () => {
     console.log('user disconnected');
   });
-  socket.on('chat message', (msg) => {
+  
+  socket.on('chat message', async (msg) => {
     console.log('message: ' + msg);
-    io.emit('chat message', msg);
+    try {
+      // Save message to database
+      const newMessage = new Message({ content: msg });
+      await newMessage.save();
+      
+      // Emit the message and its new database _id to everyone
+      io.emit('chat message', newMessage.content, newMessage._id.toString());
+    } catch (e) {
+      console.error('Error saving message:', e);
+    }
   });
 });
 
---
+++ Websockets/socket-chat-example/src/db.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const messageSchema = new mongoose.Schema({
+  content: String,
+});
+
+export const Message = mongoose.model('Message', messageSchema);
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect('mongodb://127.0.0.1:27017/socket_chat');
+    console.log('MongoDB connected');
+  } catch (err) {
+    console.error('MongoDB connection error:', err);
+  }
+};
