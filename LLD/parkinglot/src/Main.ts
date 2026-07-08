d8132c0c4bfb6f026579150f7822248823fd63df
imshubhamgiri <skgiri569@gmail.com>
2026-07-08 20:07:27 +0530

    parking lot project learning

---
+++ LLD/parkinglot/src/Main.ts
@@ -0,0 +1,50 @@
+import ParkingLot from "./ParkingLot"
+import ParkingFloor from "./Floor"
+import ParkingSpot from "./Spot"
+import { Car } from "./Vehicle"
+
+const parkYourCar = ParkingLot.getInstance("ParkYourCar")
+console.log('check', parkYourCar.name)
+console.log('end')
+
+const car1 = new Car("car-1")
+const car2 = new Car("car-2")
+const car3 = new Car("car-3")
+const car4 = new Car("car-4")
+
+const a1 = new ParkingSpot('a-1', "Car")
+// const a2 = new ParkingSpot('a-2', "Truck")
+const a3 = new ParkingSpot('a-3', "Bike")
+const a4 = new ParkingSpot('a-4', "Car")
+// const a5 = new ParkingSpot('a-5', "Car")
+
+
+const f1 = new ParkingFloor("1")
+f1.addSpot(a1)
+// f1.addSpot(a2)
+f1.addSpot(a3)
+f1.addSpot(a4)
+// f1.addSpot(a5)
+
+const b1 = new ParkingSpot('b-1', "Car")
+const b2 = new ParkingSpot('b-2', "Truck")
+// const b3 = new ParkingSpot('b-3', "Bike")
+const b4 = new ParkingSpot('b-4', "Car")
+// const b5 = new ParkingSpot('b-5', "Car")
+
+
+const f2 = new ParkingFloor("2")
+f2.addSpot(b1)
+f2.addSpot(b2)
+// f2.addSpot(b3)
+f2.addSpot(b4)
+// f2.addSpot(b5)
+
+parkYourCar.addFloor(f1)
+parkYourCar.addFloor(f2)
+parkYourCar.spotAvailability()
+
+parkYourCar.parkCar(car1)
+parkYourCar.parkCar(car2)
+parkYourCar.unparkCar(car1) //May throw a error let's see if it works yes a errro come 
+parkYourCar.spotAvailability()
---
+++ LLD/parkinglot/src/ParkingLot.ts
@@ -0,0 +1,58 @@
+import ParkingFloor from "./Floor";
+import ParkingSpot from "./Spot";
+import AbstractVehicle from "./Vehicle";
+
+class ParkingLot {
+  private static instance: ParkingLot;
+  private floors: ParkingFloor[]
+  name : string;
+  protected carSpotMap: Record<string,ParkingSpot> 
+
+  private constructor(name: string){
+    this.name = name
+    this.floors = []
+    this.carSpotMap = {}
+  }
+
+  static getInstance(name: string = "Default"): ParkingLot{
+    if(!ParkingLot.instance){
+      this.instance = new ParkingLot(name)
+    }
+    return ParkingLot.instance
+  }
+
+  parkCar(vehicle: AbstractVehicle): void{
+    for(let floor of this.floors){
+      const availableSpot = floor.findAvailableSpot(vehicle)
+      if(availableSpot){
+        availableSpot.parkCar(vehicle)
+        this.carSpotMap[vehicle.getNumber()] = availableSpot
+        console.log(`${vehicle.getNumber()} parked on ${availableSpot.spotName}`)
+        return 
+      }
+    }
+  }
+
+  unparkCar(vehicle: AbstractVehicle){
+    const spot = this.carSpotMap[vehicle.getNumber()] 
+    if(!spot){
+      throw new Error(`${vehicle.getNumber()} is not Parked.`)
+    }
+    spot.unparkCar()
+    console.log(`${vehicle.getNumber()} unparked from ${spot.spotName}`)
+    delete this.carSpotMap[vehicle.getNumber()];
+  }
+
+  spotAvailability(){
+    for(let floor of this.floors){
+      const availability = floor.reportAvailability()
+      console.log(`${floor.floor} : `, availability)
+    }
+  }
+
+  addFloor(floor : ParkingFloor){
+    this.floors.push(floor)
+  }
+}
+
+export default ParkingLot
