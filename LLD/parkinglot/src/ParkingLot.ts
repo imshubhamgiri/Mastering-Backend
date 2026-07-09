a8ab911f82f6eb389990155e2334f10cecdd4aee
imshubhamgiri <skgiri569@gmail.com>
2026-07-09 23:53:27 +0530

    console the message when someone try to make another instance from parking lot

--- LLD/parkinglot/src/ParkingLot.ts
+++ LLD/parkinglot/src/ParkingLot.ts
@@ -17,6 +17,9 @@ class ParkingLot {
   static getInstance(name: string = "Default"): ParkingLot{
     if(!ParkingLot.instance){
       this.instance = new ParkingLot(name)
+      console.log('ParkingLot instance created with name:', name)
+    }else{
+      console.log('ParkingLot instance already exists with name:', this.instance.name)
     }
     return ParkingLot.instance
   }
@@ -27,7 +30,7 @@ class ParkingLot {
       if(availableSpot){
         availableSpot.parkCar(vehicle)
         this.carSpotMap[vehicle.getNumber()] = availableSpot
-        console.log(`${vehicle.getNumber()} parked on ${availableSpot.spotName}`)
+        console.log('Car parked:', `${vehicle.getNumber()} parked on ${availableSpot.spotName}`)
         return 
       }
     }
@@ -36,17 +39,18 @@ class ParkingLot {
   unparkCar(vehicle: AbstractVehicle){
     const spot = this.carSpotMap[vehicle.getNumber()] 
     if(!spot){
+      console.log('Car not found:', `${vehicle.getNumber()} is not Parked.`)
       throw new Error(`${vehicle.getNumber()} is not Parked.`)
     }
     spot.unparkCar()
-    console.log(`${vehicle.getNumber()} unparked from ${spot.spotName}`)
+    console.log('Car unparked:', `${vehicle.getNumber()} unparked from ${spot.spotName}`)
     delete this.carSpotMap[vehicle.getNumber()];
   }
 
   spotAvailability(){
     for(let floor of this.floors){
       const availability = floor.reportAvailability()
-      console.log(`${floor.floor} : `, availability)
+      console.log('Spot availability:', `${floor.floor} : `, availability)
     }
   }
 
